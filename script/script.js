f94159ba892e3486bd16e46f8b87a05745d44dbe
Luisadrian22 <133919177+Luisadrian22@users.noreply.github.com>
2023-07-14 21:57:24 -0400

    Added new info and JavaScript to home page

--- script/script.js
+++ script/script.js
@@ -10,3 +10,61 @@ toggleBtn.onclick = function () {
     ? 'fa-solid fa-xmark'
     : 'fa-solid fa-bars'
 }
+
+// Slide-in JavaScript for home page principal item
+
+const principals = document.querySelectorAll('.principal')
+
+const options = {
+    threshold: 0.5
+  }
+
+const observerP = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-in');
+    } else {
+      entry.target.classList.remove('slide-in');
+    }
+  });
+}, options);
+
+principals.forEach(principal => {
+  observerP.observe(principal);
+})
+
+// Slide-in JavaScript for left items
+const lefts = document.querySelectorAll('.left')
+
+
+const observer1 = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-in1');
+    } else {
+      entry.target.classList.remove('slide-in1');
+    }
+  });
+}, options);
+
+lefts.forEach(left => {
+  observer1.observe(left);
+})
+
+// Slide-in JavaScript for right items
+const rights = document.querySelectorAll('.right')
+
+
+const observer2 = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-in2');
+    } else {
+      entry.target.classList.remove('slide-in2');
+    }
+  });
+}, options);
+
+rights.forEach(right => {
+  observer2.observe(right);
+})
